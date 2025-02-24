0132ade2762b43f2272a0e08897067890feb907c
Priartessbiscuit <primsmp@gmail.com>
2025-02-24 20:28:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Solicitar ao usuário que insira seus dados
+let nome = prompt("Por favor, insira seu nome:");
+let idade = prompt("Por favor, insira sua idade:");
+let cidade = prompt("Por favor, insira sua cidade:");
+
+// Exibir as informações formatadas
+console.log(Nome: ${nome}, Idade: ${idade}, Cidade: ${cidade});
